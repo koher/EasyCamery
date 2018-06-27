59229affe64564d3dd99d7edab8a8241492622f5
Yuta Koshizawa <koher@koherent.org>
2018-06-27 13:15:15 +0900

    Suppport Carthage and add sample

---
+++ Sample/ViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+import AVFoundation
+import EasyCamery
+import EasyImagy
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    private let camera: Camera<RGBA<UInt8>> = try! Camera(sessionPreset: .vga640x480, focusMode: .continuousAutoFocus)
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        camera.start { [weak self] image in
+            DispatchQueue.main.async {
+                self?.imageView.image = image.uiImage
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        camera.stop()
+        
+        super.viewWillDisappear(animated)
+    }
+}
--- Tests/EasyCameryTests/EasyCameryTests.swift
+++ Tests/EasyCameryTests/EasyCameryTests.swift
@@ -1,16 +1,28 @@
 import XCTest
 @testable import EasyCamery
 
-final class EasyCameryTests: XCTestCase {
+class EasyCameryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
     func testExample() {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(EasyCamery().text, "Hello, World!")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
 }
